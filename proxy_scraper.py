5b595ceb705e6eff44ddc205ff50f4c2d90c87df
akbarali123A <123758859+akbarali123A@users.noreply.github.com>
2025-07-28 09:09:36 +0530

    Update proxy_scraper.py

--- proxy_scraper.py
+++ proxy_scraper.py
@@ -7,7 +7,7 @@ import re
 import json
 from datetime import datetime
 import pytz
-
+timestamp = datetime.now(pytz.timezone('Asia/Kolkata')).strftime('%Y-%m-%d %H:%M:%S')
 # Configuration
 USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
